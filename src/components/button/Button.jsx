de9cc1da32e91473bccb8bdec99265c6a03e94fe
YankovWeb <yankovWeb@outlook.com>
2023-08-14 21:24:03 +0300

    create a custom button

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/prop-types */
+import styles from './button.module.css';
+
+const Button = (props) => {
+  console.log(styles);
+  const {
+    type,
+    text,
+    onClick,
+    color = 'white',
+    backgroundColor = 'black',
+    haveBorder = true,
+  } = props;
+  const typeEnum = {
+    s: styles.s,
+    m: styles.m,
+    lg: styles.lg,
+    xl: styles.xl,
+  };
+
+  return (
+    <button
+      style={{backgroundColor: `${backgroundColor}`, color: `${color}`}}
+      className={`${typeEnum[type]} ${haveBorder && styles.borderRadius}`}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default Button;
